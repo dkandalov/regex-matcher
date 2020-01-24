327f45252fc5793a9687f567effcbd5fb0066021
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-01-24 16:02:43 +0000

    empty message

--- src/test/kotlin/RegexMatcherTests.kt
+++ src/test/kotlin/RegexMatcherTests.kt
@@ -1,7 +1,12 @@
+import kotlincommon.test.shouldEqual
 import org.junit.jupiter.api.Test
 
 class RegexMatcherTests {
     @Test fun `some examples`() {
-        
+        "".matchesRegex("") shouldEqual true
     }
+}
+
+private fun String.matchesRegex(regex: String): Boolean {
+    return true
 }
